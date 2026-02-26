9dbb87ca404dc9298ddabb2c8021e131b9afe5d7
MiranKK <mirankawa2002@gmail.com>
2026-02-26 21:23:04 +0300

    Design profile screen

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:online_groceries_app_ui/provider/user_provider.dart';
+import 'package:online_groceries_app_ui/style/custom_text_style.dart';
 import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -9,22 +11,59 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final userprovider = Provider.of<UserProvider>(context);
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () async {
-            await userprovider.signout();
-            if (userprovider.errormessage == null) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text("Sign out successfully")),
+      body: Column(
+        children: [
+          Row(
+            spacing: 4.w,
+            children: [
+              Image.asset("assets/images/profile-img.png", width: 80.w),
+
+              Column(
+                children: [
+                  Text(
+                    "Miran Kawa",
+                    style: CustomTextStyle().xsmallBlackBoldText,
+                  ),
+                  Text(
+                    "mirankawa93@gmail.com",
+                    style: CustomTextStyle().xxsmallBlackBoldText.copyWith(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+
+          Container(
+            height: 600,
+            child: ListView.builder(itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                
+                ),
               );
-            } else {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text(userprovider.errormessage!)),
-              );
-            }
-          },
-          child: const Text("Sign out"),
-        ),
+            },),
+          ),
+          Center(
+            child: TextButton(
+              onPressed: () async {
+                await userprovider.signout();
+                if (userprovider.errormessage == null) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Sign out successfully")),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(userprovider.errormessage!)),
+                  );
+                }
+              },
+              child: const Text("Sign out"),
+            ),
+          ),
+        ],
       ),
     );
   }
